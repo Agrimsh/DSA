84c9e688c46d1ff1f669a4a58c2891c30c786182
Agrimsh <162814128+Agrimsh@users.noreply.github.com>
2025-12-10 23:13:31 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (6.64%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -11,9 +11,29 @@ class Solution {
         if(dp[idx]!= -1){
             return dp[idx];
         }
-        int firstnum=arr[idx]+helper(arr,idx-2,dp);
+        int firstnum=arr[idx]+helper(arr,idx-2,dp);// n-1 to 0
         int secondnum=helper(arr,idx-1,dp);
         dp[idx]=Math.max(firstnum,secondnum);
         return dp[idx];
     }
 }
+
+/*class Solution {
+    public int rob(int[] arr) {
+        int dp[]=new int[arr.length+1];
+        Arrays.fill(dp,-1);
+        return helper(arr,0,dp);   
+    }
+    public int helper(int arr[],int idx,int dp[]){   //0 to n
+        if(idx>=arr.length){
+            return 0;
+        }
+        if(dp[idx]!= -1){
+            return dp[idx];
+        }
+        int firstnum=arr[idx]+helper(arr,idx+2,dp);
+        int secondnum=helper(arr,idx+1,dp);
+        dp[idx]=Math.max(firstnum,secondnum);
+        return dp[idx];
+    }
+}*/
