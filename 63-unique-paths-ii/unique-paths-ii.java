818766cc94f4c88bee7fbf94a13095e750053ade
Agrimsh <162814128+Agrimsh@users.noreply.github.com>
2025-12-12 15:47:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (64.87%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int dp[][]=new int [grid.length][grid[0].length];
+        for(int [] r:dp){
+            Arrays.fill(r,-1);
+        }
+        return helper(grid,0,0,dp);  
+    }
+    int helper(int grid[][],int i,int j,int dp[][]){
+        if(i>=grid.length || j>=grid[0].length || grid[i][j]==1){
+            return 0;
+        }
+        if(i==grid.length-1 && j==grid[0].length-1){
+            return 1;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int down=helper(grid,i+1,j,dp);
+        int right=helper(grid,i,j+1,dp);
+        return dp[i][j]=down+right;
+        
+    }
+}
