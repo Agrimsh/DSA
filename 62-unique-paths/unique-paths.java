d70288566c20fb2d08de7b5e3d2c1f332b8fdbfb
Agrimsh <162814128+Agrimsh@users.noreply.github.com>
2025-12-12 12:01:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (14.12%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,26 +1,24 @@
 class Solution {
-
     public int uniquePaths(int m, int n) {
+        int [][]grid=new int[m][n];
         int dp[][]=new int[m][n];
-         for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        for (int r[]:dp){
+            Arrays.fill(r,-1);
         }
-        return solve(m-1,n-1,dp);   
+       return helper(0,0,m,n,grid,dp);
     }
-
-     int solve(int i, int j, int dp[][]) {
-        if (i < 0 || j < 0 )
+    public int helper(int i,int j,int m,int n,int grid[][],int dp[][]){
+        if(i==m || j==n){
             return 0;
-
-        if (i == 0 && j == 0) {
+        }
+        if(i==m-1 && j==n-1){
             return 1;
         }
-        if (dp[i][j]!=-1){
+        if(dp[i][j]!=-1){
             return dp[i][j];
         }
-
-        int up=solve(i - 1, j,dp);
-        int left=solve(i, j - 1,dp); 
-        return dp[i][j]=up+left;
+        int down=helper(i+1,j,m,n,grid , dp);
+        int right=helper(i,j+1,m,n,grid,dp);
+        return dp[i][j]=down+right;
     }
 }
