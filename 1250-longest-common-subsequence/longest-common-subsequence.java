982d2192bf5f2fa8899d25b18fc781ba18286487
Agrim <162814128+Agrimsh@users.noreply.github.com>
2026-01-09 19:13:16 +0530

    Time: 20 ms (78.47%) | Memory: 54.2 MB (12.66%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,25 +1,18 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int dp[][]=new int[text1.length()][text2.length()];
-        for(int i[]:dp){
-            Arrays.fill(i,-1);
+        int dp[][]=new int[text1.length()+1][text2.length()+1];
+        for(int i=text1.length()-1;i>=0;i--){
+            for(int j=text2.length()-1;j>=0;j--){
+                if(text1.charAt(i)==text2.charAt(j)){
+                     dp[i][j]=1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
         }
-       return helper(text1,text2,0,0,dp);
-        
-    }
-    public int helper(String s1,String s2,int i,int j,int dp[][]){
-        int take =0;
-        if(i==s1.length()||j==s2.length()){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s1.charAt(i)==s2.charAt(j)){
-            take=1+helper(s1,s2,i+1,j+1,dp);
-        }
-        int not_take=Math.max(helper(s1,s2,i+1,j,dp),helper(s1,s2,i,j+1,dp));// we have two cases in skip
-        return dp[i][j]=Math.max(take,not_take);
+        return dp[0][0];
+
     }
 }
     
