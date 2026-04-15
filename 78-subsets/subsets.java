8737f855c94cea3afd817c5c29943c7675b10e6c
Agrim <162814128+Agrimsh@users.noreply.github.com>
2026-04-15 20:14:52 +0530

    Time: 1 ms (88.75%) | Memory: 43.9 MB (89.60%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+          generate(nums, 0, new ArrayList<>(), ans);
+        return ans;
+
+        
+    }
+    public static void generate(int nums[],int i,List<Integer>curr,List<List<Integer>> ans){
+        if(i==nums.length){
+            ans.add(new ArrayList(curr));
+            return;
+        }
+        curr.add(nums[i]);
+        generate(nums,i+1,curr,ans);
+
+        curr.remove(curr.size()-1);
+
+        generate(nums,i+1,curr,ans);
+    }
+}
