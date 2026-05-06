68899218d75e60ca933fcc6a109471a45c6ffc80
Agrim <162814128+Agrimsh@users.noreply.github.com>
2026-05-06 09:50:19 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (92.13%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -14,16 +14,14 @@ class Solution {
             return null;
         }
         ListNode temp=head;
-        while(temp.next!=null || temp==null){
+        while(temp==null || temp.next!=null){
             if(temp.val==temp.next.val){
                 temp.next=temp.next.next;
             }
             else{
                 temp=temp.next;
             }
-
-        }
-        return head;
-        
+        } 
+        return head;       
     }
 }
