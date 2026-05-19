35c3c747ce6d9044cd58ffca4c2680855844779a
Agrim <162814128+Agrimsh@users.noreply.github.com>
2026-05-19 23:34:41 +0530

    Time: 13 ms (46.35%) | Memory: 49.4 MB (36.34%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,49 +1,50 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        List<List<int[]>> adj=new ArrayList<>();
-        for(int i=0;i<=n;i++){
+        List<List<int[]>> adj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
             adj.add(new ArrayList<>());
         }
-        for(int temp[]:times){
-            int u=temp[0];
-            int v=temp[1];
-            int w=temp[2];
-            adj.get(u).add(new int[]{v, w});
 
+        for (int i = 0; i < times.length; i++) {
+            int a = times[i][0];
+            int b = times[i][1];
+            int c = times[i][2];
+
+            adj.get(a).add(new int[] { b, c });
+            
         }
-
         PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        int dis[]=new int[n+1];
-        Arrays.fill(dis,Integer.MAX_VALUE);
+        int dis[] = new int[n+1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        dis[k] = 0;
+        q.add(new int[] { k, 0 });
 
-        dis[k]=0;
-        q.add(new int[]{k,0});
+        while (!q.isEmpty()) {
+            int curr[] = q.poll();
+            int node = curr[0];
+            int d = curr[1];
 
-        while(!q.isEmpty()){
-            int curr[]=q.poll();
-            int node=curr[0];
-            int d=curr[1];
+            if (d > dis[node])
+                continue;
 
-            if (d > dis[node]) continue; 
+            for (int neigh[] : adj.get(node)) {
+                int newnode = neigh[0];
+                int newd = neigh[1];
 
-            for(int x[]: adj.get(node)){
-                int newnode=x[0];
-                int newd=x[1];
-
-                if(dis[node]+newd<dis[newnode]){ 
-                   dis[newnode]=dis[node]+newd;
-                   q.add(new int[]{newnode,dis[newnode]}); 
+                if (dis[node] + newd < dis[newnode]) {
+                    dis[newnode] = dis[node] + newd;
+                    q.add(new int[] { newnode, dis[newnode] });
                 }
             }
         }
-        int max=0;
-        for(int i=1;i<=n;i++){
-            if(dis[i]==Integer.MAX_VALUE){
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dis[i] == Integer.MAX_VALUE) {
                 return -1;
             }
-            max= Math.max(max,dis[i]);
+            max = Math.max(max, dis[i]);
         }
         return max;
-        
+
     }
 }
