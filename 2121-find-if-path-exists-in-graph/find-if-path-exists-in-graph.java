a1b86b90f9a67c96ec8177907d4c4457c9695985
Agrim <162814128+Agrimsh@users.noreply.github.com>
2026-05-24 14:00:00 +0530

    Time: 111 ms (60.21%) | Memory: 276 MB (79.14%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,35 +1,40 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>>adj=new ArrayList<>();
-        for(int i=0;i<n;i++){
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-        for(int i=0;i<edges.length;i++){
-            int a=edges[i][0];
-            int b=edges[i][1];
+
+        for (int i = 0; i < edges.length; i++) {
+            int a = edges[i][0];
+            int b = edges[i][1];
+
             adj.get(a).add(b);
             adj.get(b).add(a);
-
         }
-        boolean vis[]=new boolean[adj.size()];
-        vis[source]=true;
-        bfs(source,adj,vis);
-        if(vis[destination]==true)return true;
-        else return false;
-        
+        boolean visited[] = new boolean[n + 1];
+        bfs(source,adj,visited);
+        if (visited[destination] == true) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
-    public void bfs(int src,List<List<Integer>>adj, boolean vis[]){
-        Queue<Integer>q=new LinkedList<>();
-        vis[src]=true;
+
+    public void bfs(int src, List<List<Integer>> adj, boolean visited[]) {
+        Queue<Integer> q = new LinkedList<>();
+        visited[src] = true;
         q.add(src);
-        while(!q.isEmpty()){
-            int curr=q.poll();
-            for(int x:adj.get(curr)){
-                if(!vis[x]){
-                    q.add(x);
-                    vis[x]=true;
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            for (int neigh : adj.get(curr)) {
+                if (visited[neigh] == false) {
+                    q.add(neigh);
+                    visited[neigh] = true;
                 }
             }
         }
+
     }
 }
