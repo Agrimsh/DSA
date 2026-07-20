208cbda4221c0dfa3e2cffcde7dbc45c272ee839
Agrim <162814128+Agrimsh@users.noreply.github.com>
2026-07-20 18:08:18 +0530

    Time: 114 ms (12.99%) | Memory: 56.1 MB (10.37%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -1,38 +1,53 @@
 class Solution {
-    int[][] dp;
 
     public int longestStrChain(String[] words) {
-        Arrays.sort(words,(a, b) -> a.length() - b.length());
-        dp = new int[words.length][words.length + 1];
-        for (int[] r : dp) Arrays.fill(r, -1);
-        return helper(words, 0, -1);
+
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+        int n = words.length;
+
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return helper(words, 0, -1, dp);
     }
 
-    public int helper(String[] words, int i, int prev) {
-        if(i == words.length){
+    boolean isPredecessor(String small, String large) {
+
+        if (large.length() != small.length() + 1)
+            return false;
+
+        int i = 0, j = 0;
+
+        while (i < small.length() && j < large.length()) {
+
+            if (small.charAt(i) == large.charAt(j))
+                i++;
+
+            j++;
+        }
+
+        return i == small.length();
+    }
+
+    int helper(String[] words, int i, int prev, int[][] dp) {
+
+        if (i == words.length)
             return 0;
-        }
 
-        if (dp[i][prev + 1] != -1) return dp[i][prev + 1];
-        int not_take = helper(words, i + 1, prev);
+        if (dp[prev + 1][i] != -1)
+            return dp[prev + 1][i];
+
         int take = 0;
-        if (prev == -1 || ispredecessor(words[i], words[prev])) {
-            take = 1 + helper(words, i + 1, i);
-        }
-        return dp[i][prev + 1] = Math.max(take, not_take);
-    }
 
-    boolean ispredecessor(String a, String b) {
-        if (a.length()!= b.length() + 1) return false;
-
-        int i = 0;
-        int j = 0;
-        while (i < a.length() && j < b.length()) {
-            if (a.charAt(i) == b.charAt(j)) {
-                j++;
-            }
-            i++;
+        if (prev == -1 || isPredecessor(words[prev], words[i])) {
+            take = 1 + helper(words, i + 1, i, dp);
         }
-        return j == b.length();
+
+        int notTake = helper(words, i + 1, prev, dp);
+
+        return dp[prev + 1][i] = Math.max(take, notTake);
     }
 }
